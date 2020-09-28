b534521a3a05a3b77f04651198b9ffc584d10734
toto <toto@canonical.com>
2020-09-28 18:29:09 +0100

    Add music

--- cdrom.py
+++ cdrom.py
@@ -3,26 +3,58 @@
 import os, time
 import signal
 import sys
+from multiprocessing import Process
 
-def signal_handler(sig, frame):
-    sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-
-os.system('clear')
-
+os.system("clear")
 BASE_DIR = os.getenv("SNAP", ".") + "/"
 
-def animate(delay = 1, repeat = 10):
+p1 = None
+p2 = None
+
+
+def animate():
     frames = []
     for i in range(23):
         with open(f"{BASE_DIR}frames/frame{i+1}", "r", encoding="utf8") as f:
             frames.append(f.readlines())
 
-    for repeat in range(repeat):
+    for repeat in range(1000000):
         for frame in frames:
             print("".join(frame))
-            time.sleep(delay)
-            os.system('clear')
+            time.sleep(0.4)
+            os.system("clear")
 
-animate(delay=0.4)
+
+def sing():
+    import pyttsx3
+
+    engine = pyttsx3.init()
+
+    rate = engine.getProperty("rate")
+    engine.setProperty("rate", rate - 50)
+
+    with open(f"{BASE_DIR}frames/lyrics", "r") as ifile:
+        for line in ifile:
+            engine.say(line)
+            engine.runAndWait()
+
+    p1.terminate()
+
+
+def signal_handler(sig, frame):
+    p1.terminate()
+    p2.terminate()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+if __name__ == "__main__":
+    p1 = Process(target=animate)
+    p1.start()
+    p2 = Process(target=sing)
+    p2.start()
+    p1.join()
+    p2.join()
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="cdrom",
     version="disc-compact",
+    install_requires=requirements,
     scripts=["cdrom.py"],
 )
